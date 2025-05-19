e89f2bb85941ba1679e40c01625ae70e6c5c173c
AvadatiPendurkar <avadatipendurkar@gmail.com>
2025-05-19 11:05:17 +0530

    home changes

--- LibraryMgntSystem/src/app/home/home.component.ts
+++ LibraryMgntSystem/src/app/home/home.component.ts
@@ -2,9 +2,10 @@ import { Component } from '@angular/core';
 import { LoginComponent } from '../login/login.component';
 import { CommonModule } from '@angular/common';
 import { RouterModule, RouterOutlet } from '@angular/router';
+import { SidebarComponent } from '../admin_folder/sidebar/sidebar.component';
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [SidebarComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
